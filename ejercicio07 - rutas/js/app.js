acb0ec30d9aff93af52097ce556a64356230e3cc
Eder Ibáñez Rojo <equ_406@hotmail.com>
2019-10-10 10:07:04 +0200

    EJ07- Añadida la alerta.

--- ejercicio07 - rutas/js/app.js
+++ ejercicio07 - rutas/js/app.js
@@ -1,6 +1,37 @@
-var app = angular.module('angularApp',['ngRoute']);
+var app = angular.module('angularApp',['ngRoute', 'ngSanitize']);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+
+    this.$onInit = function(){
+
+        console.log('mainCtrl onInit()');
+
+        $scope.alerta = {
+            'texto': '<strong>Lorem</strong> <hr> Ipsum',
+            'clase': 'info'
+        }
+
+        // Para saber si el servivio REST está levantado.
+        let url = 'http://localhost:3000/profile';
+        $http.get(url)
+            .then(function (result) {
+                console.trace('Servicio REST funcionando. %o', result);
+                $scope.alerta = {
+                    'texto': '<strong>¡Perfecto!</strong> Servicio REST funcionando correctamentre.',
+                    'clase': 'info'
+                }
+            }).catch(function (response) {
+                console.trace('Servicio REST fallando. %o', response);
+                $scope.alerta = {
+                    'texto': '<strong>¡Atención!</strong> Servicio REST está parado. correctamentre.',
+                    'clase': 'danger'
+                }
+            
+            });
+        
+
+
+    }; // onInit
 
 
 }]);
