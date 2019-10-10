1ff62baf652619bbd4c32ca300a0128995949afc
Eder Ibáñez Rojo <equ_406@hotmail.com>
2019-10-10 10:06:30 +0200

    EJ07 - Añadir la alerta.

---
+++ ejercicio07 - rutas/components/alert/componente-alert.js
@@ -0,0 +1,33 @@
+angular.
+    module('angularApp').
+    component('componenteAlert', {  // <componente-alert> </componente-alert>
+        templateUrl: './components/alert/template.html',
+        bindings: {
+            pclase: '@',
+            ptexto: '@'
+        },
+        controller: function AlertController() {
+
+            console.trace('componenteAlert');
+
+            $self = this;
+            $Self.alerta = {};
+
+            this.alerta = {
+                'texto': 'success',
+                'clase': 'success'
+            };
+
+            // TODO cargar los valores de la alerta al inicio de la aplicación.
+
+            this.onInit = function() {
+
+                console.trace('componenteAlert onInit()');
+                $self.alerta = {
+                    'texto': $self.ptexto,
+                    'clase': $self.pclase
+                };
+            } //onInit
+            
+        } // AlertController
+    });
